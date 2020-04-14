be61f44fc35f66e0a4dccb18a5657871f19d29a7
Fernanda Dehays <37628126+Fernandadp@users.noreply.github.com>
2020-04-14 23:32:36 +0200

    js navigation through pages

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-//const landing = document.getElementById('landing');
-//const discover = document.getElementById('discover');
-//const join = document.getElementById('join');
-//const footer = document.getElementById('footer');
-
-function myFunction() {
-    var x = document.getElementById("mobileMenu");
+function showMobileMenu(id) {
+    var x = document.getElementById(id);
     var nav = document.getElementById("mobileIcon");
-    
+
     if (x.style.display === "block") {
         x.style.display = "none";
         nav.classList.remove("open");
@@ -15,3 +10,28 @@ function myFunction() {
         nav.classList.add("open");
     }
 }
+
+function showDiv(id) {
+    var landing = document.getElementById('landing');
+    var discover = document.getElementById('discover');
+    var join = document.getElementById('join');
+    var footer = document.getElementById('footer');
+
+
+    if (id === 'discover') {
+        landing.style.display = 'none';
+        discover.style.display = 'flex';
+        join.style.display = 'none';
+        footer.style.display = 'flex';
+    } else if (id === 'join') {
+        landing.style.display = 'none';
+        discover.style.display = 'none';
+        join.style.display = 'flex';
+        footer.style.display = 'flex';
+    } else {
+        landing.style.display = 'flex';
+        discover.style.display = 'none';
+        join.style.display = 'none';
+        footer.style.display = 'none';
+    }
+};
